4e5e4855e14b9cd62d01ea0b654b41315d1da02f
ChidinmaMba <mbachidinma4@gmail.com>
2026-04-03 10:15:45 -0400

    set VITE_API_URL

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// #region agent log
+fetch("http://127.0.0.1:7287/ingest/33a6545c-95fa-4ea6-9ed7-b6eaf9d3be21", {
+  method: "POST",
+  headers: { "Content-Type": "application/json", "X-Debug-Session-Id": "9108f8" },
+  body: JSON.stringify({
+    sessionId: "9108f8",
+    hypothesisId: "H1",
+    location: "main.jsx:entry",
+    message: "bundle executed; SPA shell should load",
+    data: { path: typeof window !== "undefined" ? window.location.pathname : "" },
+    timestamp: Date.now(),
+  }),
+}).catch(() => {});
+// #endregion
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
